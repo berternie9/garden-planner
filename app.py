90094e2cd4cb7739592abc73ac61edfcbe9fa1ba
berternie9 <136939278+berternie9@users.noreply.github.com>
2024-04-20 17:08:29 +1000

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///project.db")
+db = SQL("sqlite:///database.sql")
 
 
 @app.after_request
